600f631462ba2929668fb08d4604b1a1c1546e31
Paul Welp <paul.welp@gmail.com>
2019-03-07 13:31:01 -0600

    Updates

---
+++ precache-manifest.f3ecd1bd2c12a7e8ef9d3dd9caa871f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6966c4092890a65f58bb",
+    "url": "/fontdemo/static/css/main.5f030010.chunk.css"
+  },
+  {
+    "revision": "6966c4092890a65f58bb",
+    "url": "/fontdemo/static/js/main.6966c409.chunk.js"
+  },
+  {
+    "revision": "df9a8622f2b522757823",
+    "url": "/fontdemo/static/js/runtime~main.df9a8622.js"
+  },
+  {
+    "revision": "ac164eeb7fe39897100f",
+    "url": "/fontdemo/static/js/2.ac164eeb.chunk.js"
+  },
+  {
+    "revision": "18a741c7406141e310a14d98876f7938",
+    "url": "/fontdemo/index.html"
+  }
+];
